b998e4af14207c3c2b2b927640ee8c27d95b6399
Alina Sul <48648714+Alina-sul@users.noreply.github.com>
2021-01-01 13:02:43 +0200

    style changes in top row

--- app/src/Components/pages/Dashboard/TopRow/TopRow.jsx
+++ app/src/Components/pages/Dashboard/TopRow/TopRow.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import './TopRow.sass';
+import './TopRow.scss';
 
 function TopRow() {
   return (
